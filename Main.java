f00a5123f5482e05a30895e85722a0674f0e934e
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-11-08 22:52:06 +0000

    Here we Read student names in from scanner and save to array.

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };// Defined seat numbers.
     String[] studentNames = new String[15]; // Defined an array for student names.
-    
+    Scanner sc = new Scanner(System.in); 
+    //Read student names in from scanner and save to array.
+    for(int x=0; x < 15; x++)
+    {
+      String name = sc.nextLine();
+      studentNames[x]= name; 
+    }
   }
 }
