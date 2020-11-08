00ea8656ac7c252be0f554b699856ecbb87b280d
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-11-08 22:38:44 +0000

    I defined seat numbers here.

--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
 class Main {
   public static void main(String[] args) {
     int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };// Defined seat numbers.
-
-    for (int i = 9; i >= 0; i--) {
-      if (numbers[i] % 2 == 0){
-        System.out.println(numbers[i]);
-      }
-    }
   }
 }
-  
