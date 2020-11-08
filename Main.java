55b9417c4d17ddd5fa9d835c79d3a9e270866561
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-11-08 22:40:40 +0000

    Here in this line I defined an array for student names.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
     int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };// Defined seat numbers.
+    String[] studentNames = new String[15]; // Defined an array for student names.
+    
   }
 }
